4ecdd2c199960844fbe06661ca7505719b235a3f
TonnyNyauke <nyaukeindustries@gmail.com>
2023-10-31 09:23:29 +0300

    New changes

---
+++ otrmusda/src/HomePage/Family.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Family() {
+  return (
+    <div>
+        <div className = 'family'>
+        <p>
+        Always joyful, full of humour, playful, yet also committed to the service of God, this family
+        will put a smile on any face, and bring joy and gladness to every heart. Our numerous social events
+        are always an occassion and as you are entertained by these events, we want you to feel and be part of us. 
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Family
--- otrmusda/src/HomePage/HomePage.js
+++ otrmusda/src/HomePage/HomePage.js
@@ -3,29 +3,18 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div className='homepage'>
-      <div className = 'welcome'>
+      <div className = 'aboutOTR'>
         <p>
-        Welcome to  OTR family, home far away from home.
-        We love to have you as part of our family, and may you 
-        be blessed as you join and interact with this great family.
+        Welcome to  OTR family,
+        We love to have you as part of our family.
         </p>
         <button class = 'aboutOtr'>About OTR</button>
       </div>
-      <div className = 'ourservices'>
-        <p>
-        Rich music and music, Bible studies (BS), Podcasts, Sermons, Round Table Talks (RTT),
-        family and relationships, e.t.c, OTR has services that will uplift your soul from a world
-        of care to heavenly atmosphere. You will be blessed, and be glad to be part of this family.
-        </p>
+      <div className='ourServices'>
         <button class = 'services'>Our Services</button>
       </div>
-      <div className = 'family'>
-        <p>
-        Always joyful, full of humour, playful, yet also committed to the service of God, this family
-        will put a smile on any face, and bring joy and gladness to every heart. Our numerous social events
-        are always an occassion and as you are entertained by these events, we want you to feel and be part of us. 
-        </p>
-        <button class ='thefamily'>Meet the family</button>
+      <div className='theFamily'>
+      <button class ='thefamily'>Meet the family</button>
       </div>
     </div>
   )
