10a0b814f701baf5d97efd06ab1fd9f7f016003e
TonnyNyauke <nyaukeindustries@gmail.com>
2023-10-31 12:02:19 +0300

    First draft of front end

---
+++ otrmusda.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function otrmusda() {
+  return (
+    <div>otrmusda
+    <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
+    <div style={{width: 116, height: 16, left: 83, top: 20, position: 'absolute', color: 'black', fontSize: 12, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>OLD TIME RELIGION</div>
+    <div style={{width: 319, height: 78, left: 15, top: 95, position: 'absolute', color: 'black', fontSize: 12, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word'}}>Welcome to  OTR family, home far away from home.<br/>we love to have you as part of our family, and may you <br/>be blessed as you join and interact with this great family.<br/></div>
+    <div style={{width: 87, height: 20, padding: 10, left: 235, top: 147, position: 'absolute', background: '#BBB9B9', borderRadius: 20, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{width: 79, height: 14, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+            <div style={{color: 'black', fontSize: 12, fontFamily: 'Playfair Display', fontWeight: '700', letterSpacing: 2.40, wordWrap: 'break-word'}}>About OTR</div>
+        </div>
+    </div>
+    </div>
+</div>
+  )
+}
+
+export default otrmusda
+
--- otrmusda/src/HomePage/HomePage.js
+++ otrmusda/src/HomePage/HomePage.js
@@ -1,20 +1,41 @@
 import React from 'react'
+import waterfallMoment from '../Photos/waterfall_moment.jpg';
 
 const HomePage = () => {
   return (
     <div className='homepage'>
       <div className = 'aboutOTR'>
+        <h4>NICE TO MEET YOU</h4>
         <p>
         Welcome to  OTR family,
-        We love to have you as part of our family.
+        We love to have you as part of us,
+        and may you be blessed as you join this
+        great family.
         </p>
-        <button class = 'aboutOtr'>About OTR</button>
+        <button class = 'aboutOtrBtn'>About OTR</button>
       </div>
       <div className='ourServices'>
+        <h4>WHAT WE DO</h4>
+        <p>
+          Round Table Talks (RTT), Family and Relationships, Live Podcasts,
+          Rich Gospel music, Bible Studies and many more; our services are
+          meant to lift your soul from a world of care to an atmosphere of peace,
+          comfort and love.
+        </p>
         <button class = 'services'>Our Services</button>
       </div>
       <div className='theFamily'>
-      <button class ='thefamily'>Meet the family</button>
+        <div className='watermoment'>
+        <h4 className='meetthefamily'>MEET THE FAMILY</h4>
+          <img src={waterfallMoment} alt='Waterfall Moment'></img>
+        </div>
+        <p>
+          Jovial, humorous, and a happy family;
+          you will not want to be separated from them.
+          Our fun moments bond us together, and they will never fail
+          to put a smile on your face.
+        </p>
+        <button class ='thefamily'>Meet the family</button>
       </div>
     </div>
   )
